aa5021bbed38ba6e3b4d40bc7e46cc7ae3bd49ef
MoonGoblin <bregas.sat1@gmail.com>
2025-05-19 19:28:39 +0800

    Initial Commit

---
+++ Boat3D/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Boat3D
+//
+//  Created by Bregas Satria Wicaksono on 19/05/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
