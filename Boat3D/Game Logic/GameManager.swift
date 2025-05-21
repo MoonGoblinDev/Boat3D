477c14bdd6b65f3f4d9bc6ae1e8d31e88a005a9b
MoonGoblin <bregas.sat1@gmail.com>
2025-05-21 11:51:04 +0800

    Gameplay logic (1)

---
+++ Boat3D/Game Logic/GameManager.swift
@@ -0,0 +1,7 @@
+//
+//  GameManager.swift
+//  Boat3D
+//
+//  Created by Bregas Satria Wicaksono on 21/05/25.
+//
+
---
+++ Boat3D/Game Logic/PhysicsCategory.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  Boat3D
+//
+//  Created by Bregas Satria Wicaksono on 21/05/25.
+//
+
